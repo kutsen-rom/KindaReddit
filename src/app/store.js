b5a6ecfe5d530304d9d8ab1d13087daed926aefa
navsi100rom <navsi100rom@users.noreply.github.com>
2022-08-26 00:43:51 +0300

    Delete searchBarSlice, start SearchBar.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import searchBarReducer from '../features/searchBar/searchBarSlice'
 
 export const store = configureStore({
   reducer: {
-    searchBar: searchBarReducer
   },
 });
--- src/features/searchBar/SearchBar.js
+++ src/features/searchBar/SearchBar.js
@@ -1,13 +1,22 @@
 //! author, created_utc, title, id, num_comments, permalink, score, subreddit_name_prefixed, url, 
 //* preview.images.0.source.url
 
+import { Link, Outlet } from "react-router-dom"
+import { useState } from "react";
 export const SearchBar = () => {
 
-return(
-    <div>
-        <h1>Start</h1>
-    </div>
-)
+const [ term, setTerm ] = useState('');
+
+	return(
+		<div className="search-bar">
+			<Link to='/'><div className="logo"></div></Link>
+			<form >
+				<input name="term" value={term} onChange={(e) => setTerm(e.target.value)} placeholder="Search Reddit" type='text'></input>
+			</form>
+			<button>Change theme</button>
+		<Outlet />
+		</div>
+	)
 
 
 }
